8733858975df0e864c88e0bc2e53b1238ed6d92f
heidi gouider <gouider.heidi@gmail.com>
2023-09-25 19:14:04 +0200

    ajout debut exe poo

---
+++ POO/classes/MonPerso.php
@@ -0,0 +1,7 @@
+<?php
+//je crée une instance de l'objet Personnage qui va etre utilisé sous forme d'une variable
+$perso1 = new Personnage();
+//   $perso1->setNom("Lebowski");
+//   $perso1->setPrenom("Jeff");
+
+var_dump($perso1);
---
+++ POO/classes/Personnage.class.php
@@ -0,0 +1,28 @@
+<?php
+//j'ai installé l'extension php docBlocker
+/**
+ * Objet Personnage
+ */
+class Personnage
+{
+    // Propriétés (voir getters et setters)
+    /**
+     * Identité du Personnage
+     *
+     * @var [string]
+     */
+    private $nom;
+    private $prenom;
+    /**
+     * Age du Personnage
+     *
+     * @var [int]
+     */
+    private $age;
+    /**
+     * Genre du Personnage
+     *
+     * @var [string]
+     */
+    private $sexe;
+}
